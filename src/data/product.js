4ad02b8286450fa35b4b040678d98a29888d128f
abuXahra <abdulmuminisah79@gmail.com>
2022-10-06 17:09:04 +0100

    react resturant app

--- src/data/product.js
+++ src/data/product.js
@@ -7,26 +7,26 @@ export const PRODUCTS = [
         productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
         productCategory: "FRIES"
     },
-    {
-        productImg: "http://www.jevinik.com/images/menu/chips1.jpg",
-        productName: "Fried Potato",
-        productPrice: "2500",
-        productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
-        productCategory: "FRIES"
-    },
-    {
-        productImg: "http://www.jevinik.com/images/menu/fisherman.jpg",
-        productName: "Fried Potato",
-        productPrice: "2500",
-        productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
-        productCategory: "Fish"
-    },
-    {
-        productImg: "http://www.jevinik.com/images/menu/coconutrice.jpg",
-        productName: "Coconut Rice",
-        productPrice: "2500",
-        productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
-        productCategory: "FRIES"
-    },
+    // {
+    //     productImg: "http://www.jevinik.com/images/menu/chips1.jpg",
+    //     productName: "Fried Potato",
+    //     productPrice: "2500",
+    //     productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
+    //     productCategory: "FRIES"
+    // },
+    // {
+    //     productImg: "http://www.jevinik.com/images/menu/fisherman.jpg",
+    //     productName: "Fried Potato",
+    //     productPrice: "2500",
+    //     productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
+    //     productCategory: "Fish"
+    // },
+    // {
+    //     productImg: "http://www.jevinik.com/images/menu/coconutrice.jpg",
+    //     productName: "Coconut Rice",
+    //     productPrice: "2500",
+    //     productDescription: "Porridge Beans With Assorted Protein & Fried Plantain. Kidney shaped brown beans boiled and garnished with pepper, palmoil, onion seasoning cubes and salt",
+    //     productCategory: "FRIES"
+    // },
 
 ]
